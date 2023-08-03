874e211271945bbf71f799fc1975c694d85c049e
influJr <l1e2d34@gmail.com>
2023-08-03 20:40:07 +0900

    update

--- popup.js
+++ popup.js
@@ -1,1 +1,61 @@
-const _0x54d8b0=_0x531e;function _0x531e(_0x29533f,_0x510a62){const _0x4f07d6=_0x4f07();return _0x531e=function(_0x531edb,_0x5be95b){_0x531edb=_0x531edb-0xf1;let _0x381928=_0x4f07d6[_0x531edb];return _0x381928;},_0x531e(_0x29533f,_0x510a62);}(function(_0x330008,_0x6ec9e7){const _0x3d7166=_0x531e,_0x6ac233=_0x330008();while(!![]){try{const _0x1ae2b9=-parseInt(_0x3d7166(0xf6))/0x1*(-parseInt(_0x3d7166(0x10d))/0x2)+-parseInt(_0x3d7166(0x107))/0x3+parseInt(_0x3d7166(0x103))/0x4+parseInt(_0x3d7166(0xf9))/0x5+parseInt(_0x3d7166(0xff))/0x6*(-parseInt(_0x3d7166(0xfd))/0x7)+-parseInt(_0x3d7166(0x106))/0x8+parseInt(_0x3d7166(0x104))/0x9*(parseInt(_0x3d7166(0xf3))/0xa);if(_0x1ae2b9===_0x6ec9e7)break;else _0x6ac233['push'](_0x6ac233['shift']());}catch(_0x59c97d){_0x6ac233['push'](_0x6ac233['shift']());}}}(_0x4f07,0x5ce30));function _0x4f07(){const _0x297dc2=['434OlEGtV','apple_white.jpg','40866RayNFg','apple.json','scripting','tabs','1789164eevbUQ','178047xgJaVc','click','2189896eOxEms','262929wnBAyS','getElementById','apple_black.jpg','length','then','getContext','224IGjduL','play','backgroundColor','canvas','src','drawImage','btn','getTime','addEventListener','trunc','width','white','executeScript','10YPYsQB','height','body','2234ZclBad','black','endsWith','2234220nUMJIW','style','getElementsByTagName','query'];_0x4f07=function(){return _0x297dc2;};return _0x4f07();}const btn_elm=document[_0x54d8b0(0x108)](_0x54d8b0(0x113));btn_elm[_0x54d8b0(0x115)](_0x54d8b0(0x105),()=>{const _0x470017=_0x54d8b0;if(btn_elm[_0x470017(0x111)][_0x470017(0xf8)]('apple_black.jpg'))return;btn_elm[_0x470017(0x111)]=_0x470017(0x109),document[_0x470017(0xf5)][_0x470017(0xfa)][_0x470017(0x10f)]=_0x470017(0xf7),fetch(_0x470017(0x100))[_0x470017(0x10b)](_0x5a4668=>_0x5a4668['json']())['then'](_0x55118b=>badApple(_0x55118b));});function badApple(_0x5cd3d9){const _0x45e70b=_0x54d8b0,_0x44f74c=new Audio('badapple.wav');_0x44f74c[_0x45e70b(0x10e)]();const _0x51f343=new Date()['getTime']();chrome[_0x45e70b(0x102)][_0x45e70b(0xfc)]({'active':!![],'currentWindow':!![]},function(_0x349278){const _0x342c6c=_0x45e70b;chrome[_0x342c6c(0x101)]['executeScript']({'target':{'tabId':_0x349278[0x0]['id']},'function':()=>{const _0x5adcc0=_0x342c6c,_0x32e720=document['getElementsByTagName']('canvas');for(let _0x181160=0x0;_0x181160<_0x32e720['length'];_0x181160++){_0x32e720[_0x181160][_0x5adcc0(0x117)]=_0x32e720[_0x181160][_0x5adcc0(0x117)],_0x32e720[_0x181160][_0x5adcc0(0xf4)]=_0x32e720[_0x181160][_0x5adcc0(0xf4)];}}});const _0x2da238=setInterval(()=>{const _0x521fbb=_0x342c6c,_0x453d5d=Math[_0x521fbb(0x116)]((new Date()[_0x521fbb(0x114)]()-_0x51f343)/0x64);if(_0x453d5d>_0x5cd3d9[_0x521fbb(0x10a)]){clearInterval(_0x2da238),btn_elm[_0x521fbb(0x111)]=_0x521fbb(0xfe),document[_0x521fbb(0xf5)][_0x521fbb(0xfa)][_0x521fbb(0x10f)]=_0x521fbb(0xf1);return;}chrome['scripting'][_0x521fbb(0xf2)]({'target':{'tabId':_0x349278[0x0]['id']},'function':_0x7c1169=>{const _0x5e7493=_0x521fbb,_0x4f139b=new Image();_0x4f139b['src']=_0x7c1169;const _0xee19e8=document[_0x5e7493(0xfb)](_0x5e7493(0x110));for(let _0x226c26=0x0;_0x226c26<_0xee19e8[_0x5e7493(0x10a)];_0x226c26++){const _0x37587c=_0xee19e8[_0x226c26][_0x5e7493(0x10c)]('2d');let _0x5e1602,_0x1c9cbb;_0xee19e8[_0x226c26][_0x5e7493(0x117)]/_0xee19e8[_0x226c26][_0x5e7493(0xf4)]>_0x4f139b['width']/_0x4f139b[_0x5e7493(0xf4)]?(_0x5e1602=_0x4f139b[_0x5e7493(0x117)]*_0xee19e8[_0x226c26][_0x5e7493(0xf4)]/_0x4f139b['height'],_0x1c9cbb=_0xee19e8[_0x226c26][_0x5e7493(0xf4)]):(_0x5e1602=_0xee19e8[_0x226c26][_0x5e7493(0x117)],_0x1c9cbb=_0x4f139b[_0x5e7493(0xf4)]*_0xee19e8[_0x226c26][_0x5e7493(0x117)]/_0x4f139b[_0x5e7493(0x117)]),_0x37587c[_0x5e7493(0x112)](_0x4f139b,(_0xee19e8[_0x226c26][_0x5e7493(0x117)]-_0x5e1602)/0x2,(_0xee19e8[_0x226c26]['height']-_0x1c9cbb)/0x2,_0x5e1602,_0x1c9cbb);}},'args':[_0x5cd3d9[_0x453d5d]]});},0xa);});}
+const btn_elm = document.getElementById("btn");
+
+btn_elm.addEventListener("click", () => {
+    if (btn_elm.src.endsWith("apple_black.jpg")) {
+        return;
+    }
+    btn_elm.src = "apple_black.jpg"
+    document.body.style.backgroundColor = "black";
+    fetch("apple.json")
+		.then( response => response.json())
+		.then( data => badApple(data));
+});
+
+function badApple(data) {
+    const bgm = new Audio("badapple.wav");
+    bgm.play();
+    const starttime = new Date().getTime();
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.scripting.executeScript({
+            target: { tabId: tabs[0].id },
+            function: () => {
+                const cvs = document.getElementsByTagName("canvas");
+                for (let i = 0; i < cvs.length; i++) {
+                    cvs[i].width = cvs[i].width;
+                    cvs[i].height = cvs[i].height;
+                }
+            },
+        });
+        const clearid = setInterval(() => {
+            const now = Math.trunc((new Date().getTime() - starttime) / 100 )
+            if (now > data.length){
+                clearInterval(clearid);
+                btn_elm.src = "apple_white.jpg"
+                document.body.style.backgroundColor = "white";
+                return;
+            }
+            chrome.scripting.executeScript({
+                target: { tabId: tabs[0].id },
+                function: (src) => {
+                    const img = new Image();
+                    img.src = src;
+                    const cvs = document.getElementsByTagName("canvas");
+                    for (let i = 0; i < cvs.length; i++) {
+                        const ct = cvs[i].getContext("2d");
+                        let witdh, height;
+                        if (cvs[i].width / cvs[i].height > img.width / img.height) {
+                            witdh = img.width * cvs[i].height / img.height;
+                            height = cvs[i].height;
+                        }
+                        else {
+                            witdh = cvs[i].width;
+                            height = img.height * cvs[i].width / img.width;
+                        }
+                        ct.drawImage(img, (cvs[i].width - witdh) / 2, (cvs[i].height - height) / 2, witdh, height);
+                    }
+                },
+                args: [data[now] ? data[now] : data[now - 1]]
+            });
+        }, 10);
+    });
+}
